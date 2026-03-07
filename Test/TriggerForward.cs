f54a19e0985bf19d84a4716b0ed03f1b78c0b321
laogou152 <2361752731@qq.com>
2026-03-07 13:02:06 +0800

    FSM

---
+++ Test/TriggerForward.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerForward : MonoBehaviour
+{
+    public FSM fsm; // 在 Inspector 中拖入子对象的 FSM
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            fsm?.OnPlayerEnter(other.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            fsm?.OnPlayerExit(other.transform);
+        }
+    }
+}
+
