ff0b2639a36eac48a8a78c207c71f0e9c42906ce
好想喝杨枝甘露 <149086335+AmakusaAkina@users.noreply.github.com>
2026-03-17 21:00:43 +0800

    初步搭建整体UI框架 移动Prefab文件夹到Resources中

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class UIManager
+{
+    private static UIManager instance;
+
+    private Dictionary<string, string> pathDict;
+    private Dictionary<string, GameObject> prefabDict;
+    private Dictionary<string, UIBasePanel> panelDict;
+    //  单例
+    public static UIManager Instance
+    {
+        get
+        {
+            if (Instance == null)
+            {
+                instance = new UIManager();
+            }
+            return instance;
+        }
+    }
+
+
+    private void InitDicts()
+    {
+        pathDict = new Dictionary<string, string>()
+        {
+            {UIConst.UIName, ""}
+        };
+        prefabDict = new Dictionary<string, GameObject>();
+        panelDict = new Dictionary<string, UIBasePanel>();
+    }
+    private UIManager()
+    {
+        InitDicts();
+    }
+
+    public class UIConst
+    {
+        public const string UIName = "";
+    }
+
+
+    public UIBasePanel OpenPanel(string name)
+    {
+        //  检查是否已经打开
+        UIBasePanel panel = null;
+        if (panelDict.TryGetValue(name, out panel))
+        {
+            Debug.LogError($"{name} 界面已打开");
+            return null;
+        }
+
+        //  检查路径是否存在
+        string path = "";
+        if (!pathDict.TryGetValue(name, out path))
+        {
+            Debug.LogError($"{name} 未找到对应路径");
+            return null;
+        }
+
+        //  检查是否已缓存
+        GameObject panelPrefab = null;
+        if (!prefabDict.TryGetValue(name, out panelPrefab))
+        {
+            string realPath = "Prefab/UI/" + path;
+            panelPrefab = Resources.Load<GameObject>(realPath);
+            prefabDict.Add(name, panelPrefab);
+        }
+        
+        //  正式打开界面
+        GameObject panelObject = GameObject.Instantiate(panelPrefab);
+        panel = panelObject.GetComponent<UIBasePanel>();
+        panelDict.Add(name, panel);
+        return panel;
+    }
+
+    public bool ClosePanel(string name)
+    {
+        UIBasePanel panel = null;
+        if (!panelDict.TryGetValue(name, out panel))
+        {
+            Debug.LogError($"{name} 界面未打开");
+            return false;
+        }
+
+        panel.ClosePanel();
+        panelDict.Remove(name);
+        return true;
+    }
+
+}
