203fba9176dc494adc5d161739b5b01822be1345
好想喝杨枝甘露 <149086335+AmakusaAkina@users.noreply.github.com>
2026-04-19 18:39:13 +0800

    change weaponinfo

--- Assets/Scripts/UI/WeaponInfo/UIWeaponInfo.cs
+++ Assets/Scripts/UI/WeaponInfo/UIWeaponInfo.cs
@@ -5,28 +5,40 @@ public class UIWeaponInfo : MonoBehaviour
     public UIWeaponInfoText text;
     private WeaponInfo currentWeapon;
 
-    private void Update()
+    // private void Update()
+    // {
+    //     // 获取当前武器（通过玩家身上的 PlayerWeapon 组件）
+    //     if (currentWeapon == null)
+    //     {
+    //         var player = GameObject.FindGameObjectWithTag("Player");
+    //         if (player != null)
+    //         {
+    //             var playerWeapon = player.GetComponent<PlayerWeapon>();
+    //             if (playerWeapon != null)
+    //                 currentWeapon  = playerWeapon.GetCurrentWeapon();
+    //         }
+    //     }
+
+    //     if (currentWeapon != null)
+    //     {
+    //         text.SetDisplayText("Ammo: "+ currentWeapon.CurrentAmmo.ToString());
+
+    //     }
+    //     else
+    //     {
+    //         text.SetDisplayText("0");
+    //     }
+    // }
+    #region 生命周期
+    void Awake()
     {
-        // 获取当前武器（通过玩家身上的 PlayerWeapon 组件）
-        if (currentWeapon == null)
-        {
-            var player = GameObject.FindGameObjectWithTag("Player");
-            if (player != null)
-            {
-                var playerWeapon = player.GetComponent<PlayerWeapon>();
-                if (playerWeapon != null)
-                    currentWeapon = playerWeapon.GetCurrentWeapon();
-            }
-        }
-
-        if (currentWeapon != null)
-        {
-            text.SetDisplayText("Ammo: "+ currentWeapon.CurrentAmmo.ToString());
-
-        }
-        else
-        {
-            text.SetDisplayText("0");
-        }
+        EventBus.Instance.Subscribe<WeaponChangedEvent>(OnWeaponChange);
     }
+    #endregion
+    #region 回调函数
+    void OnWeaponChange(WeaponChangedEvent evt)
+    {
+        currentWeapon = evt.newWeapon;
+    }
+    #endregion
 }
